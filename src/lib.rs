298e88128cebdbf9069af291c7aeb39af9c88ba4
Alex Way <alexwa@softcat.com>
2024-10-27 20:24:55 +0000

    chore: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,103 @@
+use extism_convert::Json;
+use extism_pdk::*;
+use serde::Deserialize;
+use std::fs;
+use toml_edit::{value, DocumentMut};
+
+#[plugin_fn]
+pub fn get_version(path: String) -> FnResult<String> {
+    let version = get_toml_value(path);
+    match version {
+        Ok(contents) => Ok(contents),
+        Err(e) => Ok(e.to_string()),
+    }
+}
+
+fn get_toml_value(path: String) -> Result<String, Error> {
+    let file_contents = std::fs::read_to_string(path)?;
+    let document = file_contents.parse::<DocumentMut>().unwrap();
+    let version = document["package"]["version"].as_str().unwrap().to_string();
+    Ok(version)
+}
+
+#[derive(Debug, Deserialize)]
+struct SetVersionRequest {
+    version: String,
+    path: String,
+}
+
+#[plugin_fn]
+pub fn set_version(input: Json<SetVersionRequest>) -> FnResult<()> {
+    update_package_section(&input.0.path, &input.0.version)?;
+
+    Ok(())
+}
+
+fn update_package_section(file_path: &str, new_version: &str) -> Result<(), Error> {
+    let contents = fs::read_to_string(file_path)?;
+
+    let mut document = contents.parse::<DocumentMut>().unwrap();
+
+    if let Some(package) = document.get_mut("package") {
+        if let Some(version) = package.get_mut("version") {
+            *version = value(new_version.to_string());
+        }
+    }
+
+    fs::write(file_path, document.to_string())?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+    use tempfile::tempdir;
+
+    use super::*;
+    use std::fs::File;
+    use std::io::Write;
+
+    #[rstest]
+    #[case(
+        "[package]\nname = \"extism-convert\"\nversion = \"0.1.0\"\n\n[dependencies]\nserde = \"1.0.213\"\n",
+        "0.1.0"
+    )]
+    fn test_get_version(#[case] input: &str, #[case] expected: &str) {
+        let dir = tempdir().unwrap();
+
+        let temp_file = dir.path().join("cargo.toml");
+        let mut file = File::create(&temp_file).unwrap();
+
+        write!(file, "{}", input).unwrap();
+
+        let version = get_toml_value(temp_file.to_str().unwrap().to_string()).unwrap();
+
+        similar_asserts::assert_eq!(version, expected);
+    }
+
+    #[rstest]
+    #[case(
+        "[package]\nname = \"extism-convert\"\nversion = \"0.1.0\"\n\n[dependencies]\nserde = \"1.0.213\"\n",
+        "1.0.0",
+        "[package]\nname = \"extism-convert\"\nversion = \"1.0.0\"\n\n[dependencies]\nserde = \"1.0.213\"\n"
+    )]
+    fn test_update_package_section(
+        #[case] input: &str,
+        #[case] new_version: &str,
+        #[case] expected: &str,
+    ) {
+        let dir = tempdir().unwrap();
+
+        let temp_file = dir.path().join("cargo.toml");
+        let mut file = File::create(&temp_file).unwrap();
+
+        write!(file, "{}", input).unwrap();
+
+        update_package_section(temp_file.to_str().unwrap(), new_version).unwrap();
+
+        let updated_contents = fs::read_to_string(temp_file).unwrap();
+
+        similar_asserts::assert_eq!(updated_contents, expected);
+    }
+}
